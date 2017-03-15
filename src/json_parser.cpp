bdfc03887071fcdc3048ec75ca05f2a572650564
Corentin Garcia <corenting@gmail.com>
2017-03-15 14:02:43 +0100

    Trim names when parsing json

--- src/json_parser.cpp
+++ src/json_parser.cpp
@@ -1,4 +1,5 @@
-#include "boost/date_time/posix_time/posix_time.hpp"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/algorithm/string.hpp>
 #include "libs/json.hpp"
 #include "json_parser.h"
 #include "date.h"
@@ -19,23 +20,30 @@ std::vector<Event> JsonHelpers::JsonToEvents(std::string jsonString) {
             boost::posix_time::ptime start = Date::DateFromIsoString(event["BeginDate"].get<std::string>());
             boost::posix_time::ptime end = Date::DateFromIsoString(event["BeginDate"].get<std::string>());
             std::string name = event["Name"].get<std::string>();
+            boost::trim(name);
 
             std::vector<std::string> teachers;
-            for (auto room: event["StaffList"])
+            for (auto teacher: event["StaffList"])
             {
-                teachers.push_back(room["Name"].get<std::string>());
+                std::string teacherName = teacher["Name"].get<std::string>();
+                boost::trim(teacherName);
+                teachers.push_back(teacherName);
             }
 
             std::vector<std::string> rooms;
             for (auto room: event["RoomList"])
             {
-                rooms.push_back(room["Name"].get<std::string>());
+                std::string roomName = room["Name"].get<std::string>();
+                boost::trim(roomName);
+                rooms.push_back(roomName);
             }
 
             std::vector<std::string> groups;
-            for (auto room: event["GroupList"])
+            for (auto group: event["GroupList"])
             {
-                groups.push_back(room["Name"].get<std::string>());
+                std::string groupName = group["Name"].get<std::string>();
+                boost::trim(groupName);
+                groups.push_back(groupName);
             }
             events.emplace_back(start, end, name, groups, teachers, rooms);
         }
